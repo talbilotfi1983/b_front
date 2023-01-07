078225a3aee2eeeedbe88e4d4de3ec14fcf63153
demo_project <tl0753662720@gmail.com>
2023-01-07 23:01:52 +0100

    update project

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ app.use(express.static(__dirname + '/'));
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: '.'});
 });
-
 app.get('/*', (req, res) => {
     res.sendFile('index.html', {root: '.'});
 });
-
 app.listen(port, () => console.log(`Start Extranet_V2 server on port : ${port} !`));
