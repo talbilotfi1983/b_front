61239272a608ac2dea2c84761462342bad3c772f
Gary <lgary@autovision.fr>
2023-02-03 14:49:33 +0100

    Mise en place de la page inscription et connection

--- src/app/modules/authentification/connection/connection.component.ts
+++ src/app/modules/authentification/connection/connection.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class ConnectionComponent implements OnInit {
   @Input() connection: any;
+  @Input() inscription: any;
   @Output() emitNoneDisplay: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
